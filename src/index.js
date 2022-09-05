7828ff016a02fcadbf88784360fc9e81ef781440
ZeGuilhermeYung <jgryung@gmail.com>
2022-09-05 14:30:45 -0300

    async function mongo

--- src/index.js
+++ src/index.js
@@ -2,17 +2,23 @@ import express, { json } from 'express';
 import cors from 'cors';
 import { MongoClient } from "mongodb";
 import dotenv from 'dotenv';
+import joi from 'joi'
 dotenv.config();
 
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 let db;
 
-mongoClient.connect().then(() => {
-	db = mongoClient.db("meu_lindo_projeto");
-});
-
 const server = express();
 server.use(cors());
 server.use(json());
+
+(async () => {
+  try {
+      await client.connect();
+      db = client.db('bate-bapo-UOL');
+  } catch (error) {
+      console.log(error);
+  }
+})();
 
 server.listen(5000);
