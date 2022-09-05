b4a5c66985aacf2f3fba7a01bb9b7a595a6a49d4
ZeGuilhermeYung <jgryung@gmail.com>
2022-09-05 14:12:46 -0300

    configuring mongo & dotenv

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import express, { json } from 'express';
 import cors from 'cors';
+import { MongoClient } from "mongodb";
 import dotenv from 'dotenv';
 dotenv.config();
+
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+let db;
+
+mongoClient.connect().then(() => {
+	db = mongoClient.db("meu_lindo_projeto");
+});
 
 const server = express();
 server.use(cors());
 server.use(json());
+
+server.listen(5000);
