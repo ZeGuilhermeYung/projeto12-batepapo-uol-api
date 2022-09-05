1d08431ba4ca3d2c0eaa653f167b63f60076fe2e
ZeGuilhermeYung <jgryung@gmail.com>
2022-09-05 13:59:21 -0300

    importing and declaring

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import express, { json } from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const server = express();
+server.use(cors());
+server.use(json());
